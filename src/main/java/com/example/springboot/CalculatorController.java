4be8345a1d7073122b2371f19fba4bc7faa3082d
k8sshu <k8sshu@outlook.com>
2021-04-19 15:12:27 +0000

    添加了一个暴力计算的 Spring Boot 控制器

---
+++ src/main/java/com/example/springboot/CalculatorController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class CalculatorController {
+        @RequestMapping("/")
+        public String index() {
+                double val = 0.001
+                for (i = 0; i < 1000000; i++) {
+                        val += Math.sqrt(val);
+                }
+                return "暴力计算中！";
+        }
+}
